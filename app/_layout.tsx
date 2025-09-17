54bacd993d73df41ce967aa9b214865505ecfe66
Opass Chang <th33ts@gmail.com>
2025-09-17 15:41:22 +0800

    Add react-native-animatable dependency to package.json and update pnpm-lock.yaml. Integrate AppKit and WagmiProvider in RootLayout for enhanced functionality.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,58 @@
 import { Stack } from "expo-router";
 
+// make sure import @walletconnect/react-native-compat before wagmi to avoid issues
+import "@walletconnect/react-native-compat";
+
+import {
+  AppKit,
+  createAppKit,
+  defaultWagmiConfig,
+} from "@reown/appkit-wagmi-react-native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { arbitrum, mainnet, polygon } from "@wagmi/core/chains";
+import { WagmiProvider } from "wagmi";
+
+
+
+// 0. Setup queryClient
+const queryClient = new QueryClient();
+
+// 1. Get projectId at https://dashboard.reown.com
+const projectId = "REOWN_PROJECT_ID_REMOVED";
+
+// 2. Create config
+const metadata = {
+  name: "AppKit RN",
+  description: "AppKit RN Example",
+  url: "https://reown.com/appkit",
+  icons: ["https://avatars.githubusercontent.com/u/179229932"],
+  redirect: {
+    native: "YOUR_APP_SCHEME://",
+    universal: "YOUR_APP_UNIVERSAL_LINK.com",
+  },
+};
+
+const chains = [mainnet, polygon, arbitrum] as const;
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+// 3. Create modal
+createAppKit({
+  projectId,
+  metadata,
+  wagmiConfig,
+  defaultChain: mainnet, // Optional
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+});
+
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <Stack />
+        <AppKit />
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
