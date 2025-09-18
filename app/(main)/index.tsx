ebcc50cc2cd8ba0e4411f71dc9fe9623084643b8
MinreyP <minreypeng@gmail.com>
2025-09-18 15:39:20 +0800

    clean up index page

--- app/(main)/index.tsx
+++ app/(main)/index.tsx
@@ -1,28 +1,16 @@
-import { useRouter } from 'expo-router';
-import { Text } from 'tamagui';
+import { MainLayout } from '@/components/global/main-layout';
+import { Link } from 'expo-router';
+import { Text, XStack, YStack } from 'tamagui';
 
 export default function Index() {
-  const router = useRouter();
-
-  const navigateToTrade = () => {
-    router.navigate('/(main)/trade/BTC-USD/(tab)/trade');
-  };
-
-  const navigateToSettings = () => {
-    router.navigate('/settings');
-  };
-
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Link href="/(main)/settings">Settings</Link>
-      <Link href="/(main)/account/(tab)/positions">Positions</Link>
-      <Text>Home screen</Text>
-    </View>
+    <MainLayout>
+      <YStack flex={1} padding="$4" space="$4">
+        <XStack justifyContent="space-between" alignItems="center"></XStack>
+        <Link href="/(main)/settings">Settings</Link>
+        <Link href="/(main)/account/(tab)/positions">Positions</Link>
+        <Text>Home screen</Text>
+      </YStack>
+    </MainLayout>
   );
 }
