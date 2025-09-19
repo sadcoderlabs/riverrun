4cab3e59c4970cfbcfb442fa7e0e9ffb6c1cff0c
MinreyP <minreypeng@gmail.com>
2025-09-19 11:26:56 +0800

    add top bar to the index

--- app/(main)/index.tsx
+++ app/(main)/index.tsx
@@ -5,13 +5,13 @@ import { YStack } from 'tamagui';
 
 export default function Index() {
   return (
-    <MainLayout noHeader={true}>
+    <MainLayout noHeader={true} showTopBar={true}>
       <YStack flex={1} padding="$4" justifyContent="center" alignItems="center" gap="$4">
         <Link href="/(main)/settings" asChild>
           <Text>Settings</Text>
         </Link>
-        <Link href="/(main)/account/(tab)/positions" asChild>
-          <Text>Positions</Text>
+        <Link href="/(main)/trade/[market]/(tab)/positions" asChild>
+          <Text>View Positions(in Trade)</Text>
         </Link>
       </YStack>
     </MainLayout>
