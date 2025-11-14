c1b06727f9a0221355d78c49871b81c7b8d5bbbc
Opass Chang <th33ts@gmail.com>
2025-11-14 15:22:47 +0800

    chore: update configuration files and add environment variable support
    - Added .env to .gitignore to prevent local environment files from being tracked.
    - Removed the app name and bundle identifier from app.json for cleaner configuration.
    - Enhanced eas.json to include APP_VARIANT for different environments (development, preview, production).
    - Updated package.json to include a new script for starting the app in development mode with the APP_VARIANT set.
    
    These changes improve the management of environment-specific settings and streamline the development process.

---
+++ app.config.ts
@@ -0,0 +1,50 @@
+import { ExpoConfig, ConfigContext } from 'expo/config';
+
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+
+const getUniqueIdentifier = (): string => {
+  if (IS_DEV) {
+    return 'com.perpetualprotocol.riverrun.dev';
+  }
+
+  if (IS_PREVIEW) {
+    return 'com.perpetualprotocol.riverrun.preview';
+  }
+
+  return 'com.perpetualprotocol.riverrun';
+};
+
+const getAppName = (): string => {
+  if (IS_DEV) {
+    return 'PERP GO (Dev)';
+  }
+
+  if (IS_PREVIEW) {
+    return 'PERP GO (Preview)';
+  }
+
+  return 'PERP GO';
+};
+
+export default ({ config }: ConfigContext): ExpoConfig => {
+  if (!config.slug) {
+    throw new Error('slug is required in app.json');
+  }
+
+  return {
+    ...config,
+    slug: config.slug,
+    // Dynamic app name based on variant
+    name: getAppName(),
+    // Dynamic bundle identifiers for iOS and Android
+    ios: {
+      ...config.ios,
+      bundleIdentifier: getUniqueIdentifier(),
+    },
+    android: {
+      ...config.android,
+      package: getUniqueIdentifier(),
+    },
+  };
+};
